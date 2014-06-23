f6596b6ac3d5f65235a7a68aef58b2d957473141
Brady Somerville <brady.somerville@gmail.com>
2014-06-23 10:29:53 -0500

    Fix asset locations in install generator for Spree 2.2

--- lib/generators/spree_videos/install/install_generator.rb
+++ lib/generators/spree_videos/install/install_generator.rb
@@ -4,11 +4,11 @@ module SpreeVideos
       source_root File.expand_path('../../../templates/', __FILE__)
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_videos\n" 
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require store/spree_videos\n" 
       end
-      
+
       # def add_stylesheets
-      #   inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_videos\n", :before => /\*\//, :verbose => true
+      #   inject_into_file "vendor/assets/stylesheets/frontend/all.css", " *= require store/spree_videos\n", :before => /\*\//, :verbose => true
       # end
 
       def copy_initializer_file
